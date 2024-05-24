2483a7044e03cc20aba17448029a9a2a102c4cbd
zubair-H <zubair.linkdin@gmail.com>
2024-05-24 00:31:11 -0400

    update 2

--- LeetiFy/src/components/Page1.jsx
+++ LeetiFy/src/components/Page1.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import './css/Page1.css';
 
-export default function page1() {
+export default function Page1() {
   return (
-    <div>
-      
-    </div> 
-  ); 
+    <div className="container">
+      <div className="box">
+        <ul className="list">
+        
+          <li id="topicArrays">Topic: <button>Arrays</button></li>
+          <li>1. Two Sum <button>+</button> <button>Link</button></li>
+          <li>2. Best Time to Buy and Sell Stock <button> +</button> <button>Link</button> </li>
+          <li>3. Contains Duplicate <button>+</button> <button>Link</button></li>
+          <li>4. Product of Array Except Self <button>+</button> <button>Link</button></li>
+          <li>5. Maximum Subarray <button>+</button> <button>Link</button></li>
+          <li>6. Maximum Product Subarray <button>+</button> <button>Link</button></li>
+          <li>7. Find Minimum in Rotated Sorted Array <button>+</button> <button>Link</button></li>
+          <li>8. Search in Rotated Sorted Array <button>+</button> <button>Link</button></li>
+          <li>9. 3 Sum <button>+</button> <button>Link</button></li>
+          <li>10. Container With Most Water <button>+</button> <button>Link</button></li>
+        </ul>
+
+      </div>
+
+       <div className="box">
+        <ul className="list">
+        
+          <li id="topicArrays">Topic: <button>Arrays</button></li>
+          <li>1. Two Sum <button>+</button> <button>Link</button></li>
+          <li>2. Best Time to Buy and Sell Stock <button> +</button> <button>Link</button> </li>
+          <li>3. Contains Duplicate <button>+</button> <button>Link</button></li>
+          <li>4. Product of Array Except Self <button>+</button> <button>Link</button></li>
+          <li>5. Maximum Subarray <button>+</button> <button>Link</button></li>
+          <li>6. Maximum Product Subarray <button>+</button> <button>Link</button></li>
+          <li>7. Find Minimum in Rotated Sorted Array <button>+</button> <button>Link</button></li>
+          <li>8. Search in Rotated Sorted Array <button>+</button> <button>Link</button></li>
+          <li>9. 3 Sum <button>+</button> <button>Link</button></li>
+          <li>10. Container With Most Water <button>+</button> <button>Link</button></li>
+        </ul>
+
+      </div>
+    
+        </div>
+  );
 }
